5f3a65f4c33fb5f795fa45a2daed4de08b0b530e
developer430 <developer@fthemes.net>
2026-05-31 01:38:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+   const menuBtn = document.getElementById("menuBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    menuBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
+
+    document.querySelectorAll(".faq-trigger").forEach((trigger) => {
+  trigger.addEventListener("click", () => {
+    const content = trigger.nextElementSibling;
+    const icon = trigger.querySelector(".faq-icon");
+
+    content.classList.toggle("active");
+
+    icon.textContent =
+      content.classList.contains("active")
+        ? "−"
+        : "+";
+  });
+});
+
+AOS.init({
+  duration: 1000,
+  once: true,
+  offset: 100,
+  easing: "ease-out-cubic"
+});
